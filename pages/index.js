bee6daecc44eacce0df5fa3c992329c52aa36b2d
taey <tkul@sertiscorp.com>
2019-09-11 22:54:08 +0700

    first commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Head from 'next/head'
+
+import "../global-style.scss"
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>Home</title>
+    </Head>
+    hello
+  </div>
+)
+
+export default Home
